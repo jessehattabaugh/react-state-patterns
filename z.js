91fb64d8e18b450f3f66227b8005fa599eac72e7
Jesse Hattabaugh <jesse.hattabaugh@planet.com>
2016-02-18 13:52:55 -0800

    defatting functions

--- z.js
+++ z.js
@@ -3,43 +3,86 @@ import {render} from 'react-dom'
 import {createStore} from 'redux'
 import {connect, Provider} from 'react-redux'
 
-const store = createStore((state = {items: ['apple', 'kiwi', 'banana']}, action) => {
-  if(action.type == 'selectItem') state.selectedItem = action.item
-  console.log(action.type)
-  return state
-})
+function reducer(state = {items: ['apple', 'kiwi', 'banana']}, action) {
+  var newState = Object.assign({}, state);
+  if (action.type == 'selectItem') {
+    newState.selectedItem = action.item;
+  }
+  return newState;
+}
 
-const selectItem = (item) => ({type: 'selectItem', item: item})
+const store = createStore(reducer);
+
+function selectItem(item) {
+  return {
+    type: 'selectItem',
+    item: item
+  };
+}
 
 const List = connect(
-  (state) => ({items: state.items, selectedItem: state.selectedItem}),
-  (dispatch) => ({onSelect: (item) => dispatch(selectItem(item))})
-)
-  (({items, onSelect, selectedItem}) => {
-    console.log(selectedItem)
-    return <ul>
-      {items.map(item => {
-        console.log(selectedItem, item)
-        return <li key={item} onClick={() => onSelect(item)} style={{textDecoration: selectedItem == item ? 'underline' : 'none'}}>{item}</li>
-      })}
-    </ul>
-})
+  function (state) {
+    return {
+      items: state.items,
+      selectedItem: state.selectedItem
+    };
+  },
+  function (dispatch) {
+    return {
+      onSelect: function (item) {
+        dispatch(selectItem(item));
+      }
+    };
+  }
+)(renderList);
+
+function renderList({items, onSelect, selectedItem}) {
+  console.log(selectedItem);
+  return <ul>
+    {items.map(function (item) {
+      console.log(selectedItem, item)
+      return <li
+        key={item}
+        onClick={function () {
+          onSelect(item);
+        }}
+        style={{textDecoration: selectedItem == item ? 'underline' : 'none'}}>
+          {item}
+        </li>
+    })}
+  </ul>;
+}
 
 const Dialog = connect(
-  (state) => ({selectedItem: state.selectedItem}),
-  (dispatch) => ({onClose: () => dispatch(selectItem())})
-)
-  (({selectedItem, onClose}) => <div>
+  function (state) {
+    return {
+      selectedItem: state.selectedItem
+    };
+  },
+  function (dispatch) {
+    return {
+      onClose: function () {
+        dispatch(selectItem());
+      }
+    };
+  }
+)(renderDialog);
+
+function renderDialog({selectedItem, onClose}) {
+  return <div>
     <a onClick={onClose}>close</a>
     {selectedItem}
-  </div>)
+  </div>;
+}
 
-const App = () => <main>
-  <List/>
-  <Dialog/>
-</main>
+function App() {
+  return <main>
+    <List/>
+    <Dialog/>
+  </main>;
+}
 
 render(
   <Provider store={store}><App/></Provider>,
   document.body.appendChild(document.createElement("DIV"))
-)
+);
